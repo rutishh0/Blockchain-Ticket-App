c72c0576998f64b1948d1139c09dc883fec80337
ucabzol <ucabzol@ucl.ac.uk>
2024-12-05 12:28:55 +0000

    Update IConditionalFundRelease.sol

--- contracts/interfaces/IConditionalFundRelease.sol
+++ contracts/interfaces/IConditionalFundRelease.sol
@@ -13,5 +13,14 @@ event FundsDeposited (address indexed depositor, uint256 amount);
 event FundsReleased (address indexed beneficicary, uint256 amount);
 event ConditionUpdated (ConditionStatus status);
 
+modifier onlyOwner () {
+    require(msg.sender == owner, "Only owner can perform this action");
+    _;
+}
+
+modifier inState (ConditionStatus _state) {
+    require(conditionStatus == _state, "Invalid state for this action");
+
+
 
 }

a1ab784bc8d7ca2241e3d6776698af7c3a5cd88a
ucabzol <ucabzol@ucl.ac.uk>
2024-12-05 12:07:56 +0000

    Update IConditionalFundRelease.sol

--- contracts/interfaces/IConditionalFundRelease.sol
+++ contracts/interfaces/IConditionalFundRelease.sol
@@ -2,4 +2,16 @@
 pragma solidity ^0.8.20;
 
 interface IConditionalFundRelease {
+address public owner;
+address public beneficiary;
+uint256 public amount;
+
+enum ConditionsStatus {NotMet, Met, Released}
+ConditionStatus public conditionsStatus;
+
+event FundsDeposited (address indexed depositor, uint256 amount);
+event FundsReleased (address indexed beneficicary, uint256 amount);
+event ConditionUpdated (ConditionStatus status);
+
+
 }

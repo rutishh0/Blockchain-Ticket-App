4b326a60f66835b6df4e64b0003d380f6b0ea4e3
Rutishkrishna Srinivasaraghavan <ucabr02@ucl.ac.uk>
2024-12-09 19:34:41 +0000

    Fix 3 Hardhat

--- contracts/core/TicketFactory.sol
+++ contracts/core/TicketFactory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 interface IWaitlistManager {
     function getNextWaitingUser(uint256 eventId, uint256 zoneId) external view returns (address);
